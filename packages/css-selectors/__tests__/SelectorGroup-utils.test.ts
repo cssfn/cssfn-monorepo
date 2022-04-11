85f96fcddbe78a101ff51bb28a178acea5fa171b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-12 06:29:18 +0700

    updated test

---
+++ packages/css-selectors/__tests__/SelectorGroup-utils.test.ts
@@ -0,0 +1,285 @@
+import {
+    // parses:
+    parseSelectors,
+    
+    
+    
+    // SelectorEntry creates & tests:
+    parentSelector,
+    universalSelector,
+    attrSelector,
+    elementSelector,
+    idSelector,
+    classSelector,
+    pseudoClassSelector,
+    pseudoElementSelector,
+    
+    isAttrSelectorOf,
+    isElementSelectorOf,
+    isIdSelectorOf,
+    isClassSelectorOf,
+    isPseudoClassSelectorOf,
+    isClassOrPseudoClassSelectorOf,
+    isPseudoElementSelectorOf,
+    isElementOrPseudoElementSelectorOf,
+    
+    
+    
+    // Selector creates & tests:
+    Selector,
+    selector,
+    SelectorGroup,
+    selectorGroup,
+    
+    isNotEmptySelectors,
+    countSelectors,
+    
+    
+    
+    // renders:
+    selectorsToString,
+    
+    
+    
+    // transforms:
+    FlatMapSelectorsCallback,
+    flatMapSelectors,
+} from '../src/css-selectors'
+
+
+
+const replaceDivToSpan: FlatMapSelectorsCallback = (selectorEntry) => {
+    if (isElementSelectorOf(selectorEntry, 'div')) {
+        return elementSelector('span');
+    } // if
+    
+    return selectorEntry;
+};
+const replaceExpensiveToVeryCheap: FlatMapSelectorsCallback = (selectorEntry) => {
+    if (isClassSelectorOf(selectorEntry, 'expensive')) {
+        return selector(
+            classSelector('very'),
+            classSelector('cheap'),
+        );
+    } // if
+    
+    return selectorEntry;
+};
+const removeUnusedThing: FlatMapSelectorsCallback = (selectorEntry) => {
+    if (isClassSelectorOf(selectorEntry, 'unused')) {
+        return selector(
+            /* empty */
+        );
+    } // if
+    if (isAttrSelectorOf(selectorEntry, 'thing')) {
+        return selector(
+            /* empty */
+        );
+    } // if
+    
+    return selectorEntry;
+};
+
+
+
+test(`flatMapSelectors(empty)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            /* empty */
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(empty)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            selector(
+                /* empty */
+            ),
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(empty)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            selector(
+                /* empty */
+            ),
+            selector(
+                /* empty */
+            ),
+            selector(
+                /* empty */
+            ),
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(falsy)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            undefined,
+            selector(
+                /* empty */
+            ),
+            null,
+            selector(
+                /* empty */
+            ),
+            false,
+            selector(
+                /* empty */
+            ),
+            true,
+            selector(
+                /* empty */
+            ),
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(falsy)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            undefined,
+            selector(
+                undefined,
+            ),
+            null,
+            selector(
+                null,
+            ),
+            false,
+            selector(
+                false,
+            ),
+            true,
+            selector(
+                true,
+            ),
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(falsy)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            undefined,
+            null,
+            false,
+            true,
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(falsy)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`flatMapSelectors(falsy)`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        selectorGroup(
+            undefined,
+            null,
+            false,
+            true,
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+            undefined,
+            null,
+            false,
+            true,
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+            undefined,
+            null,
+            false,
+            true,
+        ),
+        replaceDivToSpan
+    )))
+    .toBe(
+        ''
+    );
+});
+
+
+
+test(`flatMapSelectors()`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        parseSelectors(
+            '.product>div>:first-child'
+        )!,
+        replaceDivToSpan
+    )))
+    .toBe(
+        '.product>span>:first-child'
+    );
+});
+test(`flatMapSelectors()`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        parseSelectors(
+            '.product.expensive>#list'
+        )!,
+        replaceExpensiveToVeryCheap
+    )))
+    .toBe(
+        '.product.very.cheap>#list'
+    );
+});
+test(`flatMapSelectors()`, () => {
+    expect(selectorsToString(flatMapSelectors(
+        parseSelectors(
+            '.product.unused>#some[thing="bleh"]:valid'
+        )!,
+        removeUnusedThing
+    )))
+    .toBe(
+        '.product>#some:valid'
+    );
+});
