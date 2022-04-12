8d5dd24b6baad6c6cc976d08399ef3b32f33e7df
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-12 17:05:30 +0700

    updated test

--- packages/css-selectors/__tests__/SelectorGroup-utils.test.ts
+++ packages/css-selectors/__tests__/SelectorGroup-utils.test.ts
@@ -5,16 +5,25 @@ import {
     
     
     // SelectorEntry creates & tests:
+    parentSelector,
+    universalSelector,
+    attrSelector,
     elementSelector,
+    idSelector,
     classSelector,
     pseudoClassSelector,
+    pseudoElementSelector,
     
     isParentSelector,
     
     isAttrSelectorOf,
     isElementSelectorOf,
+    isIdSelectorOf,
     isClassSelectorOf,
     isPseudoClassSelectorOf,
+    isClassOrPseudoClassSelectorOf,
+    isPseudoElementSelectorOf,
+    isElementOrPseudoElementSelectorOf,
     
     combinator,
     
@@ -23,8 +32,13 @@ import {
     
     
     // Selector creates & tests:
+    Selector,
     selector,
+    SelectorGroup,
     selectorGroup,
+    
+    isNotEmptySelectors,
+    countSelectors,
     
     
     
@@ -36,6 +50,11 @@ import {
     // transforms:
     FlatMapSelectorsCallback,
     flatMapSelectors,
+    GroupSelectorOptions,
+    groupSelectors,
+    groupSelector,
+    ungroupSelector,
+    ungroupSelectors,
 } from '../src/css-selectors'
 
 
@@ -348,4 +367,255 @@ test(`flatMapSelectors()`, () => {
     .toBe(
         '.parent:yeah>.sub+next, .ultra.parent:yeah:deep #field+:nth-child(2n+3), #this:is(#very.parent:yeah.exciting>.thing, ::backdrop[title="you & me"])'
     );
+});
+
+
+
+test(`groupSelectors(empty)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            /* empty */
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(empty)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            selector(
+                /* empty */
+            ),
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(empty)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            selector(
+                /* empty */
+            ),
+            selector(
+                /* empty */
+            ),
+            selector(
+                /* empty */
+            ),
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(falsy)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            undefined,
+            selector(
+                /* empty */
+            ),
+            null,
+            selector(
+                /* empty */
+            ),
+            false,
+            selector(
+                /* empty */
+            ),
+            true,
+            selector(
+                /* empty */
+            ),
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(falsy)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            undefined,
+            selector(
+                undefined,
+            ),
+            null,
+            selector(
+                null,
+            ),
+            false,
+            selector(
+                false,
+            ),
+            true,
+            selector(
+                true,
+            ),
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(falsy)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            undefined,
+            null,
+            false,
+            true,
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(falsy)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+test(`groupSelectors(falsy)`, () => {
+    expect(selectorsToString(groupSelectors(
+        selectorGroup(
+            undefined,
+            null,
+            false,
+            true,
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+            undefined,
+            null,
+            false,
+            true,
+            selector(
+                undefined,
+                null,
+                false,
+                true,
+            ),
+            undefined,
+            null,
+            false,
+            true,
+        )
+    )))
+    .toBe(
+        ''
+    );
+});
+
+
+
+const groupList = ['is','not','has','where'];
+groupList.forEach((group) => {
+    [false, true].forEach((cancelSingular) => {
+        const options : GroupSelectorOptions = {
+            selectorName          : group,
+            cancelGroupIfSingular : cancelSingular,
+        };
+        
+        test(`groupSelectors()`, () => {
+            expect(selectorsToString(groupSelectors(
+                parseSelectors(
+                    `.product>div>:first-child`
+                )!,
+                options
+            )))
+            .toBe(
+                cancelSingular
+                ?
+                `.product>div>:first-child`
+                :
+                `:${group}(.product>div>:first-child)`
+            );
+        });
+        test(`groupSelectors()`, () => {
+            expect(selectorsToString(groupSelectors(
+                parseSelectors(
+                    `.product.expensive>#list, ::backdrop:hover, ::before, ::after`
+                )!,
+                options
+            )))
+            .toBe(
+                cancelSingular
+                ?
+                `.product.expensive>#list, ::backdrop:hover, ::before, ::after`
+                :
+                `:${group}(.product.expensive>#list), ::backdrop:hover, ::before, ::after`
+            );
+        });
+        test(`groupSelectors()`, () => {
+            expect(selectorsToString(groupSelectors(
+                parseSelectors(
+                    `::backdrop:hover, ::before, ::after`
+                )!,
+                options
+            )))
+            .toBe(
+                `::backdrop:hover, ::before, ::after`
+            );
+        });
+        test(`groupSelectors()`, () => {
+            expect(selectorsToString(groupSelectors(
+                parseSelectors(
+                    `.product.unused>#some[thing="bleh"]:valid+:garbage:first-child`
+                )!,
+                options
+            )))
+            .toBe(
+                cancelSingular
+                ?
+                `.product.unused>#some[thing="bleh"]:valid+:garbage:first-child`
+                :
+                `:${group}(.product.unused>#some[thing="bleh"]:valid+:garbage:first-child)`
+            );
+        });
+        test(`groupSelectors()`, () => {
+            expect(selectorsToString(groupSelectors(
+                parseSelectors(
+                    `.ultra :deep #field+:nth-child(2n+3), #this:is(#very .exciting .thing), ::backdrop+:hover`
+                )!,
+                options
+            )))
+            .toBe(
+                `:${group}(.ultra :deep #field+:nth-child(2n+3), #this:is(#very .exciting .thing)), ::backdrop+:hover`
+            );
+        });
+        test(`groupSelectors()`, () => {
+            expect(selectorsToString(groupSelectors(
+                parseSelectors(
+                    `&>.sub+next, .ultra&:deep #field+:nth-child(2n+3), #this:is(#very&.exciting>.thing), ::backdrop[title="you & me"]`
+                )!,
+                options
+            )))
+            .toBe(
+                `:${group}(&>.sub+next, .ultra&:deep #field+:nth-child(2n+3), #this:is(#very&.exciting>.thing)), ::backdrop[title="you & me"]`
+            );
+        });
+    });
 });
