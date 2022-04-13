2ef1022c0d57cef169882fbbffb95774dbad5acf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-14 03:11:28 +0700

    updated css-types

--- packages/css-types/src/css-types.ts
+++ packages/css-types/src/css-types.ts
@@ -28,7 +28,7 @@ export type CssSimpleValue                  =
 //#region generic complex values
 export type CssComplexBaseValueOf<TValue>   =
     | TValue                                               // final_value
-    | CustomCssRef                                         // css_variable
+    | CssCustomRef                                         // css_variable
 export type CssComplexSingleValueOf<TValue> =
     | CssComplexBaseValueOf<TValue>                        // single_value
     | [CssComplexBaseValueOf<TValue>, '!important']        // single_value with !important
@@ -46,60 +46,64 @@ export type CssComplexValueOf<TValue>       =
 
 
 //#region css custom properties
+// declaration:
 export type CssCustomName         = `--${string}`
 
-export type CustomCssSingleRef    = `var(${CssCustomName})`
-export type CustomCssRef          = CustomCssSingleRef|`var(${CssCustomName},${CustomCssSingleRef})`|`var(${CssCustomName},${string})`
-export type CustomCssKeyframesRef = (string & {})
+// values:
+export type CssCustomKeyframesRef = (string & {})           // special value, not included to `CssCustomRef`
+export type CssCustomSingleRef    = `var(${CssCustomName})` // single value
+export type CssCustomRef          =
+    | CssCustomSingleRef                                    // single value
+    | `var(${CssCustomName}, ${CssCustomSingleRef})`        // with shallow fallback
+ // | `var(${CssCustomName}, ${CssCustomRef})`              // with nested  fallback(s), can't circular
+ // | `var(${CssCustomName}, ${string})`                    // use string for nested fallback(s), not ideal but works
 
+export type CssCustomValue        = CssComplexValueOf<CssSimpleValue>
 
-export type CustomCssValue        = CssComplexValueOf<CssSimpleValue>
-
-export interface CustomCssProps {
-    [name: CssCustomName] : CustomCssValue
+export interface CssCustomProps {
+    [name: CssCustomName] : CssCustomValue
 }
 //#endregion css custom properties
 
 
 
-//#region standard css properties
+//#region css known (standard) properties
 export type CssLength   = (string & {}) | 0
 export type CssDuration = (string & {})
 
-export type BasicKnownCssProps<TLength = CssLength, TDuration = CssDuration> = Properties<TLength, TDuration>
+export type CssKnownBaseProps<TLength = CssLength, TDuration = CssDuration> = Properties<TLength, TDuration>
 
-export type KnownCssName         = keyof BasicKnownCssProps
-export type KnownCssValueOf
-    <TName extends KnownCssName> = Exclude<BasicKnownCssProps[TName], undefined|null>
+export type CssKnownName         = keyof   CssKnownBaseProps
+export type CssKnownValueOf
+    <TName extends CssKnownName> = Exclude<CssKnownBaseProps[TName], undefined|null>
 
 
 
-export type KnownCssPropsOf
-    <TName extends KnownCssName> = {
-    [name in TName ] ?: KnownCssValueOf<name>
+export type CssKnownPropsOf<TName extends CssKnownName> = {
+    [name in TName] ?: CssKnownValueOf<name>
 }
-export type KnownStandardLonghandCssProps  = KnownCssPropsOf<keyof StandardLonghandProperties>
-export type KnownStandardShorthandCssProps = KnownCssPropsOf<keyof StandardShorthandProperties>
-export type KnownStandardCssProps          = KnownStandardLonghandCssProps & KnownStandardShorthandCssProps
+export type CssKnownStandardLonghandProps  = CssKnownPropsOf<keyof StandardLonghandProperties>
+export type CssKnownStandardShorthandProps = CssKnownPropsOf<keyof StandardShorthandProperties>
+export type CssKnownStandardProps          = CssKnownStandardLonghandProps & CssKnownStandardShorthandProps
 
-export type KnownVendorLonghandCssProps    = KnownCssPropsOf<keyof VendorLonghandProperties>
-export type KnownVendorShorthandCssProps   = KnownCssPropsOf<keyof VendorShorthandProperties>
-export type KnownVendorCssProps            = KnownVendorLonghandCssProps & KnownVendorShorthandCssProps
+export type CssKnownVendorLonghandProps    = CssKnownPropsOf<keyof VendorLonghandProperties>
+export type CssKnownVendorShorthandProps   = CssKnownPropsOf<keyof VendorShorthandProperties>
+export type CssKnownVendorProps            = CssKnownVendorLonghandProps & CssKnownVendorShorthandProps
 
-export type KnownObsoleteCssProps          = KnownCssPropsOf<keyof ObsoleteProperties>
+export type CssKnownObsoleteProps          = CssKnownPropsOf<keyof ObsoleteProperties>
 
-export type KnownSvgCssProps               = KnownCssPropsOf<keyof SvgProperties>
+export type CssKnownSvgProps               = CssKnownPropsOf<keyof SvgProperties>
 
-export type KnownCssProps                  = KnownStandardCssProps & KnownVendorCssProps & KnownObsoleteCssProps & KnownSvgCssProps
-//#endregion standard css properties
+export type CssKnownProps                  = CssKnownStandardProps & CssKnownVendorProps & CssKnownObsoleteProps & CssKnownSvgProps
+//#endregion css known (standard) properties
 
 
 
 //#region cssfn properties
-export type CssKeyframes  = Dictionary<{}> // TODO: <Style>
-export type GeneralCssValue = (string & {}) | (number & {}) | CssKeyframes
-export type CssValue      = undefined | null | GeneralCssValue | GeneralCssValue[] | (GeneralCssValue|GeneralCssValue[]|'!important')[]
+export type CssKeyframes    = Dictionary<{}> // TODO: <Style>
+export type CssGeneralValue = (string & {}) | (number & {}) | CssKeyframes
+export type CssValue        = undefined | null | CssGeneralValue | CssGeneralValue[] | (CssGeneralValue|CssGeneralValue[]|'!important')[]
 
-export interface CssProps extends KnownCssProps, CustomCssProps {
+export interface CssProps extends CssKnownProps, CssCustomProps {
 }
 //#endregion cssfn properties
