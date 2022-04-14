e77f6fc315fefb0331cd97ff1a9f4fc8d14624a2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-15 02:31:20 +0700

    added cssfn

---
+++ packages/cssfn/__tests__/cssfn.test.ts
@@ -0,0 +1,5 @@
+import {
+} from '../src/cssfn'
+
+
+
---
+++ packages/cssfn/src/cssfn.ts
@@ -0,0 +1,11 @@
+// cssfn:
+import type {
+    Dictionary,
+}                           from '@cssfn/types'
+import type {
+    CssCustomName,
+    CssCustomRef,
+}                           from '@cssfn/css-types'
+
+
+
