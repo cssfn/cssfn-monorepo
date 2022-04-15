65fa05c9f38807c24bce4a227b0d8d03d9ee0f78
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-15 09:26:03 +0700

    updated cssfn

--- packages/cssfn/src/cssfn.ts
+++ packages/cssfn/src/cssfn.ts
@@ -1,11 +1,18 @@
 // cssfn:
 import type {
-    Dictionary,
+    // types:
+    Factory,
+    ProductOrFactory,
 }                           from '@cssfn/types'
 import type {
-    CssCustomName,
-    CssCustomRef,
+    // cssfn properties:
+    CssClassList,
+    CssScopeName,
+    CssScopeMap,
 }                           from '@cssfn/css-types'
 
 
 
+export const createUseSheet = <TCssScopeName extends CssScopeName = CssScopeName>(classes: ProductOrFactory<CssClassList<TCssScopeName>>, sheetId?: string): Factory<CssScopeMap<TCssScopeName>> => {
+    throw Error('under construction');
+}
