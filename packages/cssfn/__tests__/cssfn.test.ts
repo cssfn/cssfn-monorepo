6dab41256a7c4fe853a3e55f4dbe2c67e8b720fa
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-15 09:54:21 +0700

    updated cssfn

--- packages/cssfn/__tests__/cssfn.test.ts
+++ packages/cssfn/__tests__/cssfn.test.ts
@@ -1,5 +1,22 @@
 import {
+    // style sheets:
+    styleSheet,
+    
+    
+    
+    // scopes:
+    scopeOf,
+    mainScope,
+    globalScope,
 } from '../src/cssfn'
 
 
 
+styleSheet(() => [
+    mainScope(
+    ),
+    scopeOf('menuBar',
+    ),
+    globalScope(
+    ),
+]);
--- packages/cssfn/src/cssfn.ts
+++ packages/cssfn/src/cssfn.ts
@@ -6,13 +6,42 @@ import type {
 }                           from '@cssfn/types'
 import type {
     // cssfn properties:
+    CssRuleCollection,
+    
+    CssStyleCollection,
+    
+    CssClassEntry,
     CssClassList,
+    
     CssScopeName,
     CssScopeMap,
 }                           from '@cssfn/css-types'
 
 
 
-export const createUseSheet = <TCssScopeName extends CssScopeName = CssScopeName>(classes: ProductOrFactory<CssClassList<TCssScopeName>>, sheetId?: string): Factory<CssScopeMap<TCssScopeName>> => {
-    throw Error('under construction');
+// style sheets:
+export const styleSheet  = <TCssScopeName extends CssScopeName = CssScopeName>(classes: ProductOrFactory<CssClassList<TCssScopeName>>, sheetId?: string): Factory<CssScopeMap<TCssScopeName>> => {
+    return () => ({} as CssScopeMap<TCssScopeName>);
 }
+
+
+
+// scopes:
+/**
+ * Defines an additional scoped styleSheet.
+ * @returns A `CssClassEntry` represents a scoped styleSheet.
+ */
+export const scopeOf     = <TCssScopeName extends CssScopeName = CssScopeName>(scopeName: TCssScopeName, ...styles: CssStyleCollection[]): CssClassEntry<TCssScopeName> => [
+    scopeName,
+    styles
+];
+/**
+ * Defines the main styleSheet.
+ * @returns A `CssClassEntry` represents a main styleSheet.
+ */
+export const mainScope   = (...styles: CssStyleCollection[]) => scopeOf('main' , ...styles);
+/**
+ * Defines an unscoped styleSheet (applied to a whole document).
+ * @returns A `CssClassEntry` represents an unscoped styleSheet.
+ */
+export const globalScope = (...rules :  CssRuleCollection[]) => scopeOf(''     , ...rules );
