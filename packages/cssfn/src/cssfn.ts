d6dbf4b5c6738f206b4aa6a2f8fd7cec2d559e59
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-16 08:31:31 +0700

    updated cssfn

--- packages/cssfn/src/cssfn.ts
+++ packages/cssfn/src/cssfn.ts
@@ -14,6 +14,7 @@ import type {
     CssClassList,
     
     CssScopeName,
+    CssScopeEntry,
     CssScopeMap,
 }                           from '@cssfn/css-types'
 
@@ -28,19 +29,19 @@ export {
 // scopes:
 /**
  * Defines an additional scoped styleSheet.
- * @returns A `CssClassEntry` represents a scoped styleSheet.
+ * @returns A `CssScopeEntry` represents a scoped styleSheet.
  */
-export const scopeOf     = <TCssScopeName extends CssScopeName>(scopeName: TCssScopeName, ...styles: CssStyleCollection[]): CssClassEntry<TCssScopeName> => [
+export const scopeOf     = <TCssScopeName extends CssScopeName>(scopeName: TCssScopeName, ...styles: CssStyleCollection[]): CssScopeEntry<TCssScopeName> => [
     scopeName,
     styles
 ];
 /**
  * Defines the main styleSheet.
- * @returns A `CssClassEntry` represents a main styleSheet.
+ * @returns A `CssScopeEntry` represents a main styleSheet.
  */
 export const mainScope   = (...styles: CssStyleCollection[]) => scopeOf('main' , ...styles);
 /**
  * Defines an unscoped styleSheet (applied to a whole document).
- * @returns A `CssClassEntry` represents an unscoped styleSheet.
+ * @returns A `CssScopeEntry` represents an unscoped styleSheet.
  */
 export const globalScope = (...rules :  CssRuleCollection[]) => scopeOf(''     , ...rules );
