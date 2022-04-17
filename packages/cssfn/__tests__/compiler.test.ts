c6d9bfea762e133a67b7979b6af78f9f45ff0e97
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-18 02:54:10 +0700

    updated test

--- packages/cssfn/__tests__/compiler.test.ts
+++ packages/cssfn/__tests__/compiler.test.ts
@@ -2077,7 +2077,7 @@ test(`mergeStyles([{empty}])`, () => {
     );
 });
 
-test(`mergeStyles([{empty}])`, () => {
+test(`mergeStyles([{empty}...{empty}])`, () => {
     expect(mergeStyles([
         {},
         {},
@@ -2088,7 +2088,7 @@ test(`mergeStyles([{empty}])`, () => {
     );
 });
 
-test(`mergeStyles([{empty}|falsy])`, () => {
+test(`mergeStyles([{empty}|falsy...{empty}|falsy])`, () => {
     expect(mergeStyles([
         {},
         undefined,
@@ -2105,4 +2105,474 @@ test(`mergeStyles([{empty}|falsy])`, () => {
     );
 });
 //#endregion test with empty style(s)
+
+
+
+//#region test with single style
+test(`mergeStyles({some})`, () => {
+    const mainStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    };
+    expect(mergeStyles(mainStyle))
+    .toEqual(mainStyle);
+    expect(mergeStyles([mainStyle]))
+    .toEqual(mainStyle);
+});
+
+test(`mergeStyles({some+symbols})`, () => {
+    const symbol1 = Symbol('symbol1');
+    const symbol2 = Symbol('symbol2');
+    const mainStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+        paddingInline: '2rem',
+    };
+    expect(mergeStyles(mainStyle))
+    .toEqual(mainStyle);
+    expect(mergeStyles([mainStyle]))
+    .toEqual(mainStyle);
+});
+//#endregion test with single style
+
+
+
+//#region test with some style(s)
+test(`mergeStyles({some}, {empty})`, () => {
+    const mainStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    };
+    const addStyle: CssStyle = {
+        /* empty */
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    });
+});
+
+test(`mergeStyles({empty}, {some})`, () => {
+    const mainStyle: CssStyle = {
+        /* empty */
+    };
+    const addStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    });
+});
+
+test(`mergeStyles({some+symbols}, {empty})`, () => {
+    const symbol1 = Symbol('symbol1');
+    const symbol2 = Symbol('symbol2');
+    const mainStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+        paddingInline: '2rem',
+    };
+    const addStyle: CssStyle = {
+        /* empty */
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+    });
+});
+
+test(`mergeStyles({empty}, {some+symbols})`, () => {
+    const symbol1 = Symbol('symbol1');
+    const symbol2 = Symbol('symbol2');
+    const mainStyle: CssStyle = {
+        /* empty */
+    };
+    const addStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+        paddingInline: '2rem',
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+    });
+});
+
+test(`mergeStyles({some}, {some})`, () => {
+    const mainStyle: CssStyle = {
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+    };
+    const addStyle: CssStyle = {
+        background: 'red',
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        background: 'red',
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    });
+});
+
+test(`mergeStyles({some+symbols}, {some+symbols})`, () => {
+    const symbol1 = Symbol('symbol1');
+    const symbol2 = Symbol('symbol2');
+    const symbol3 = Symbol('symbol3');
+    const symbol4 = Symbol('symbol4');
+    const mainStyle: CssStyle = {
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        color: 'blue',
+        opacity: 0.5,
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+        border: [['solid', '2px', 'dashed']],
+    };
+    const addStyle: CssStyle = {
+        background: 'red',
+        [symbol3]: {
+            justifySelf: 'stretch',
+            flex: [[0, 0, 'auto']],
+        },
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        [symbol4]: {
+            cursor: 'pointer',
+            opacity: 0.9,
+        },
+        paddingInline: '2rem',
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        background: 'red',
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+        [symbol3]: {
+            justifySelf: 'stretch',
+            flex: [[0, 0, 'auto']],
+        },
+        [symbol4]: {
+            cursor: 'pointer',
+            opacity: 0.9,
+        },
+    });
+});
+
+test(`mergeStyles({conflict}, {conflict})`, () => {
+    const mainStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+    };
+    const addStyle: CssStyle = {
+        appearance: 'none',
+        opacity: 0.85,
+        display: 'flex',
+        boxShadow: [
+            ['10px', '5px', '5px', 'black'],
+            ['30px', '20px', 'purple'],
+        ],
+        background: 'black',
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        color: 'blue',
+        border: [['solid', '2px', 'dashed']],
+        paddingInline: '2rem',
+        
+        appearance: 'none',
+        opacity: 0.85,
+        display: 'flex',
+        boxShadow: [
+            ['10px', '5px', '5px', 'black'],
+            ['30px', '20px', 'purple'],
+        ],
+        background: 'black',
+    });
+});
+
+test(`mergeStyles({conflict+symbols}, {conflict+symbols})`, () => {
+    const symbol1 = Symbol('symbol1');
+    const symbol2 = Symbol('symbol2');
+    const symbol3 = Symbol('symbol3');
+    const symbol4 = Symbol('symbol4');
+    const mainStyle: CssStyle = {
+        background: 'red',
+        color: 'blue',
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        opacity: 0.5,
+        border: [['solid', '2px', 'dashed']],
+        boxShadow: [
+            ['10px', '5px', '5px', 'red'],
+            ['60px', '-16px', 'teal'],
+            ['inset', '5em', '1em', 'gold'],
+            ['3px', '3px', 'red'],
+            ['-1em', '0', '.4em', 'olive'],
+        ],
+        paddingInline: '2rem',
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+    };
+    const addStyle: CssStyle = {
+        appearance: 'none',
+        opacity: 0.85,
+        display: 'flex',
+        [symbol3]: {
+            justifySelf: 'stretch',
+            flex: [[0, 0, 'auto']],
+        },
+        boxShadow: [
+            ['10px', '5px', '5px', 'black'],
+            ['30px', '20px', 'purple'],
+        ],
+        [symbol4]: {
+            cursor: 'pointer',
+            opacity: 0.9,
+        },
+        background: 'black',
+    };
+    expect(mergeStyles([mainStyle, addStyle]))
+    .toEqual({
+        color: 'blue',
+        border: [['solid', '2px', 'dashed']],
+        paddingInline: '2rem',
+        
+        appearance: 'none',
+        opacity: 0.85,
+        display: 'flex',
+        boxShadow: [
+            ['10px', '5px', '5px', 'black'],
+            ['30px', '20px', 'purple'],
+        ],
+        background: 'black',
+        
+        [symbol1]: {
+            margin: '2rem',
+            minWidth: '100px',
+        },
+        [symbol2]: {
+            gap: '2rem',
+            visibility: 'hidden',
+            animation: 'none',
+        },
+        [symbol3]: {
+            justifySelf: 'stretch',
+            flex: [[0, 0, 'auto']],
+        },
+        [symbol4]: {
+            cursor: 'pointer',
+            opacity: 0.9,
+        },
+    });
+});
+//#endregion test with some style(s)
 //#endregion test mergeStyles()
