18a0de1b15f724a10002451fb758269cf97578b7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-04-27 13:10:24 +0700

    updated cssfn

--- packages/cssfn/src/renders.ts
+++ packages/cssfn/src/renders.ts
@@ -36,12 +36,18 @@ import type {
     // types:
     StyleSheet,
 }                           from './styleSheets.js'
+import {
+    mergeStyles,
+}                           from './mergeStyles.js'
 
 // other libs:
 import {
     // tests:
     default as warning,
 }                           from 'tiny-warning'
+import {
+    camelCase,
+}                           from 'camel-case'
 
 
 
@@ -59,8 +65,8 @@ const fastHash = (input: string) => {
 };
 
 const takenHashes = new Map</*hash :*/string, /*owner :*/string>();
-export const generateId = (sheetId: string, scopeName: CssScopeName): string => {
-    const mySelf = `${sheetId}${scopeName}`;
+export const generateId = (styleSheetId: string, scopeName: CssScopeName): string => {
+    const mySelf = `${styleSheetId}${scopeName}`;
     let   myHash = fastHash(mySelf);
     
     
@@ -82,8 +88,8 @@ export const generateId = (sheetId: string, scopeName: CssScopeName): string => 
         
         // try to re-generate a unique hash by adding a counter salt (not SSR friendly):
         myHash = fastHash(`${mySelf}${counterSalt}`);
-        if ((counterSalt === 2) && (sheetId !== '')) {
-            warning(false, `[cssfn] The sheetId of ${sheetId} is not a unique ID. Please re-generate another random ID.`);
+        if ((counterSalt === 2) && (styleSheetId !== '')) {
+            warning(false, `[cssfn] The styleSheetId of ${styleSheetId} is not a unique ID. Please re-generate another random ID.`);
         } // if
     } // for
     
@@ -95,30 +101,117 @@ export const generateId = (sheetId: string, scopeName: CssScopeName): string => 
 
 
 
-class RuleList {
-    constructor() {
-        //
+type RuleEntry = readonly [CssFinalSelector, CssStyleCollection]
+class RenderRules {
+    //#region private fields
+    #rendered : string[]
+    //#endregion private fields
+    
+    //#region public fields
+    readonly result : string|null
+    //#endregion public fields
+    
+    
+    
+    //#region private methods
+    #renderPropName(propName: string): string {
+        return camelCase(propName);
     }
-    add(selector: CssFinalSelector, style: CssStyle) {
-        //
+    #renderPropValue(propValue: CssCustomValue): string {
+        return `${propValue}`;
+    }
+    #renderProp(propName: string, propValue: CssCustomValue): void {
+        const rendered = this.#rendered;
+        rendered.push(this.#renderPropName(propName));
+        rendered.push(': ');
+        rendered.push(this.#renderPropValue(propValue));
+        rendered.push(';\n');
+    }
+    #renderNested(finalStyle: CssStyle): void {
+        for (const symbolProp of Object.getOwnPropertySymbols(finalStyle)) {
+            const ruleData = finalStyle[symbolProp];
+            if (ruleData === undefined) continue;
+            const [selector, styles] = ruleData;
+            if (Array.isArray(styles)) continue;
+            if (typeof(styles) === 'function') continue;
+            if (!styles || (styles === true)) continue;
+            const nestedFinalStyle = styles;
+            
+            
+            
+            if (selector === '@fallbacks') {
+                this.#renderStyle(nestedFinalStyle);
+            }
+            else if (selector === '@global') {
+                //
+            } // if
+        } // for
+    }
+    #renderStyle(finalStyle: CssStyle): void {
+        for (const propName in finalStyle) {
+            this.#renderProp(propName, (finalStyle as any)[propName])
+        } // for
+    }
+    #renderRule(finalSelector: CssFinalSelector, finalStyle: CssStyle): void {
+        const rendered = this.#rendered;
+        rendered.push(finalSelector);
+        rendered.push(' {\n');
+        {
+            this.#renderNested(finalStyle);
+            this.#renderStyle(finalStyle);
+        }
+        rendered.push('\n}\n\n');
+    }
+    //#endregion private methods
+    
+    
+    
+    constructor(rules: RuleEntry[]) {
+        this.#rendered = [];
+        for (const [finalSelector, styles] of rules) {
+            const finalStyle = mergeStyles(styles);
+            if (!finalStyle) continue; // blank style => nothing to render
+            
+            
+            
+            this.#renderRule(finalSelector, finalStyle);
+        } // for
+        
+        
+        
+        this.result = this.#rendered.join('') || null;
     }
 }
 
 
 
-export const render = (styleSheet: StyleSheet): string => {
-    if (!styleSheet.enabled) return '';
-    
-    
-    
-    const rules = new RuleList();
+export const render = (styleSheet: StyleSheet): string|null => {
+    if (!styleSheet.enabled) return null;
     
     
     
     const scopesFactory = styleSheet.scopes;
     const scopeList = (typeof(scopesFactory) === 'function') ? scopesFactory() : scopesFactory;
-    for (const [scopeName, styles] of scopeList) {
-
-    } // for
-    return 'blah';
+    
+    const styleSheetId = styleSheet.id;
+    const classes      = styleSheet.classes;
+    
+    const rules : RuleEntry[] = scopeList.map(([scopeName, styles]): RuleEntry => {
+        // generate unique class:
+        const uniqueClass    : CssClassName     = generateId(styleSheetId, scopeName);
+        const uniqueSelector : CssFinalSelector = `.${uniqueClass}`;
+        
+        // map each scopeName => uniqueClass:
+        classes[scopeName] = uniqueClass;
+        
+        return [
+            uniqueSelector,
+            styles,
+        ];
+    });
+    
+    
+    
+    // finally, render the structures:
+    return (new RenderRules(rules)).result;
 }
