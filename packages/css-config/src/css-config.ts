1ba13f98f46c77876e3d654d771cfefde2c1a2b0
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-01 17:00:31 +0700

    updated css-config

---
+++ packages/css-config/src/css-config.ts
@@ -0,0 +1,101 @@
+// cssfn:
+import type {
+    Factory,
+    ProductOrFactory,
+    Dictionary,
+    ValueOf,
+    DictionaryOf,
+}                           from '@cssfn/types'
+import type {
+    // css custom properties:
+    CssCustomSimpleRef,
+    CssCustomRef,
+    
+    
+    
+    // cssfn properties:
+    CssKeyframes,
+    
+    CssSelector,
+}                           from '@cssfn/css-types'
+import type {
+    // utilities:
+    camelCase,
+    pascalCase,
+}                           from '@cssfn/cssfn'
+
+
+
+// general types:
+export interface CssConfigProps { }
+export type Refs     <TProps extends CssConfigProps> = { [key in keyof TProps]: CssCustomSimpleRef }
+export type Vals     <TProps extends CssConfigProps> = { [key in keyof TProps]: TProps[key]        }
+
+export interface CssConfigOptions {
+    /**
+     * The prefix name of the generated css vars.
+     */
+    prefix   ?: string
+    
+    /**
+     * The declaring location (selector) of the generated css vars.
+     */
+    selector ?: CssSelector
+}
+const defaultOptions : Required<CssConfigOptions> = {
+    prefix    : '',
+    selector  : ':root',
+}
+class LiveCssConfigOptions implements Required<CssConfigOptions> {
+    //#region private properties
+    #prefix   : string
+    #selector : CssSelector
+    
+    readonly #updatedCallback : () => void
+    //#endregion private properties
+    
+    
+    
+    //#region constructors
+    constructor(updatedCallback: () => void, options?: CssConfigOptions) {
+        this.#prefix   = options?.prefix   ?? defaultOptions.prefix;   // an empty prefix   is allowed
+        this.#selector = options?.selector || defaultOptions.selector; // an empty selector is not allowed
+        
+        this.#updatedCallback = updatedCallback;
+    }
+    //#endregion constructors
+    
+    
+    
+    //#region public properties
+    get prefix() {
+        return this.#prefix;
+    }
+    set prefix(value: string) {
+        // an empty prefix is allowed
+        if (this.#prefix === value) return; // no change => no need to update
+        
+        this.#prefix = value; // update
+        this.refresh(); // notify a css-config updated
+    }
+    
+    get selector() {
+        return this.#selector;
+    }
+    set selector(value: string) {
+        value = value || defaultOptions.selector; // an empty selector is not allowed
+        if (this.#selector === value) return; // no change => no need to update
+        
+        this.#selector = value; // update
+        this.refresh(); // notify a css-config updated
+    }
+    //#endregion public properties
+    
+    
+    
+    //#region public methods
+    refresh() {
+        this.#updatedCallback?.(); // notify a css-config updated
+    }
+    //#endregion public methods
+}
