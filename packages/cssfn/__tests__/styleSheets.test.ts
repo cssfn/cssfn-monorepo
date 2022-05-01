3824fef0782b13d973f109f6da24ca6c63b95025
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-01 09:12:10 +0700

    updated test

--- packages/cssfn/__tests__/styleSheets.test.ts
+++ packages/cssfn/__tests__/styleSheets.test.ts
@@ -19,6 +19,19 @@ import {
 
 
 
+const simulateServerSide = () => {
+    jest.resetModules();
+    
+    const oriWindow   = (typeof(window) === 'undefined'  ) ? undefined : window;
+    const oriDocument = (typeof(document) === 'undefined') ? undefined : document;
+    if (oriWindow !== undefined) {
+        (globalThis as any).window = undefined;
+    } // if
+    if (oriDocument !== undefined) {
+        (globalThis as any).document = undefined;
+        if (typeof(window) !== 'undefined') window.document = undefined as any;
+    } // if
+};
 const simulateBrowserSide = () => {
     jest.resetModules();
     
@@ -34,19 +47,155 @@ const simulateBrowserSide = () => {
         (globalThis as any).window.document = mockDocument;
     } // if
 };
-const simulateServerSide = () => {
-    jest.resetModules();
+
+
+
+jest.isolateModules(() => {
+    let styleSheet  : typeof _styleSheet  = undefined as any;
+    let scopeOf     : typeof _scopeOf     = undefined as any;
+    let mainScope   : typeof _mainScope   = undefined as any;
+    let globalScope : typeof _globalScope = undefined as any;
+    let styleSheets : typeof _styleSheets = undefined as any;
+    beforeAll(async () => {
+        simulateServerSide();
+        
+        const cssfnModule      = await import('../dist/cssfn')
+        const styleSheetModule = await import('../dist/styleSheets')
+        
+        styleSheet  = cssfnModule.styleSheet
+        scopeOf     = cssfnModule.scopeOf
+        mainScope   = cssfnModule.mainScope
+        globalScope = cssfnModule.globalScope
+        styleSheets = styleSheetModule.styleSheets
+    });
     
-    const oriWindow   = (typeof(window) === 'undefined'  ) ? undefined : window;
-    const oriDocument = (typeof(document) === 'undefined') ? undefined : document;
-    if (oriWindow !== undefined) {
-        (globalThis as any).window = undefined;
-    } // if
-    if (oriDocument !== undefined) {
-        (globalThis as any).document = undefined;
-        if (typeof(window) !== 'undefined') window.document = undefined as any;
-    } // if
-};
+    
+    
+    test('[server] test registered styleSheets = 0', () => {
+        const activeSheets : StyleSheet[] = [];
+        styleSheets.subscribe((styleSheet) => {
+            if (styleSheet.enabled) {
+                activeSheets.push(styleSheet);
+            } else {
+                const index = activeSheets.indexOf(styleSheet);
+                if (index >= 0) activeSheets.splice(index, 1);
+            } // if
+        });
+        
+        expect(activeSheets.length)
+        .toBe(0);
+    });
+    
+    test('[server] test registered styleSheets = 2', () => {
+        const sheet1 = styleSheet(() => [
+            mainScope([]),
+            scopeOf('menuBar', []),
+            globalScope([]),
+        ], { id: 'sheet1' });
+        // @ts-ignore
+        const sheet2 = styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet2', enabled: false });
+        // @ts-ignore
+        const sheet3 = styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet3' });
+        
+        // @ts-ignore
+        const mainClass    = sheet1.classes.main;
+        // @ts-ignore
+        const menuBarClass = sheet1.classes.menuBar;
+        
+        
+        const activeSheets : StyleSheet[] = [];
+        styleSheets.subscribe((styleSheet) => {
+            if (styleSheet.enabled) {
+                activeSheets.push(styleSheet);
+            } else {
+                const index = activeSheets.indexOf(styleSheet);
+                if (index >= 0) activeSheets.splice(index, 1);
+            } // if
+        });
+        
+        expect(activeSheets.length)
+        .toBe(0);
+    });
+    
+    test('[server] test registered styleSheets = 4', () => {
+        styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet4' });
+        styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet5' });
+        
+        
+        
+        const activeSheets : StyleSheet[] = [];
+        styleSheets.subscribe((styleSheet) => {
+            if (styleSheet.enabled) {
+                activeSheets.push(styleSheet);
+            } else {
+                const index = activeSheets.indexOf(styleSheet);
+                if (index >= 0) activeSheets.splice(index, 1);
+            } // if
+        });
+        
+        expect(activeSheets.length)
+        .toBe(0);
+    });
+    
+    test('[server] test registered styleSheets = 6', () => {
+        const sheet6 = styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet6' });
+        const sheet7 = styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet7' });
+        const sheet8 = styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet8', enabled: false });
+        const sheet9 = styleSheet(() => [
+            mainScope([]),
+        ], { id: 'sheet9' });
+        
+        
+        
+        const activeSheets : StyleSheet[] = [];
+        styleSheets.subscribe((styleSheet) => {
+            if (styleSheet.enabled) {
+                activeSheets.push(styleSheet);
+            } else {
+                const index = activeSheets.indexOf(styleSheet);
+                if (index >= 0) activeSheets.splice(index, 1);
+            } // if
+        });
+        
+        expect(activeSheets.length)
+        .toBe(0);
+        
+        sheet8.enabled = true;
+        
+        expect(activeSheets.length)
+        .toBe(0);
+        
+        sheet6.enabled = false;
+        sheet9.enabled = false;
+        
+        expect(activeSheets.length)
+        .toBe(0);
+        
+        sheet7.enabled = false;
+        
+        expect(activeSheets.length)
+        .toBe(0);
+        
+        sheet6.enabled = true;
+        
+        expect(activeSheets.length)
+        .toBe(0);
+    });
+});
 
 
 
@@ -243,154 +392,5 @@ jest.isolateModules(() => {
             'sheet8',
             'sheet6',
         ]);
-    });
-});
-
-
-
-jest.isolateModules(() => {
-    let styleSheet  : typeof _styleSheet  = undefined as any;
-    let scopeOf     : typeof _scopeOf     = undefined as any;
-    let mainScope   : typeof _mainScope   = undefined as any;
-    let globalScope : typeof _globalScope = undefined as any;
-    let styleSheets : typeof _styleSheets = undefined as any;
-    beforeAll(async () => {
-        simulateServerSide();
-        
-        const cssfnModule      = await import('../dist/cssfn')
-        const styleSheetModule = await import('../dist/styleSheets')
-        
-        styleSheet  = cssfnModule.styleSheet
-        scopeOf     = cssfnModule.scopeOf
-        mainScope   = cssfnModule.mainScope
-        globalScope = cssfnModule.globalScope
-        styleSheets = styleSheetModule.styleSheets
-    });
-    
-    
-    
-    test('[server] test registered styleSheets = 0', () => {
-        const activeSheets : StyleSheet[] = [];
-        styleSheets.subscribe((styleSheet) => {
-            if (styleSheet.enabled) {
-                activeSheets.push(styleSheet);
-            } else {
-                const index = activeSheets.indexOf(styleSheet);
-                if (index >= 0) activeSheets.splice(index, 1);
-            } // if
-        });
-        
-        expect(activeSheets.length)
-        .toBe(0);
-    });
-    
-    test('[server] test registered styleSheets = 2', () => {
-        const sheet1 = styleSheet(() => [
-            mainScope([]),
-            scopeOf('menuBar', []),
-            globalScope([]),
-        ], { id: 'sheet1' });
-        // @ts-ignore
-        const sheet2 = styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet2', enabled: false });
-        // @ts-ignore
-        const sheet3 = styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet3' });
-        
-        // @ts-ignore
-        const mainClass    = sheet1.classes.main;
-        // @ts-ignore
-        const menuBarClass = sheet1.classes.menuBar;
-        
-        
-        const activeSheets : StyleSheet[] = [];
-        styleSheets.subscribe((styleSheet) => {
-            if (styleSheet.enabled) {
-                activeSheets.push(styleSheet);
-            } else {
-                const index = activeSheets.indexOf(styleSheet);
-                if (index >= 0) activeSheets.splice(index, 1);
-            } // if
-        });
-        
-        expect(activeSheets.length)
-        .toBe(0);
-    });
-    
-    test('[server] test registered styleSheets = 4', () => {
-        styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet4' });
-        styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet5' });
-        
-        
-        
-        const activeSheets : StyleSheet[] = [];
-        styleSheets.subscribe((styleSheet) => {
-            if (styleSheet.enabled) {
-                activeSheets.push(styleSheet);
-            } else {
-                const index = activeSheets.indexOf(styleSheet);
-                if (index >= 0) activeSheets.splice(index, 1);
-            } // if
-        });
-        
-        expect(activeSheets.length)
-        .toBe(0);
-    });
-    
-    test('[server] test registered styleSheets = 6', () => {
-        const sheet6 = styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet6' });
-        const sheet7 = styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet7' });
-        const sheet8 = styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet8', enabled: false });
-        const sheet9 = styleSheet(() => [
-            mainScope([]),
-        ], { id: 'sheet9' });
-        
-        
-        
-        const activeSheets : StyleSheet[] = [];
-        styleSheets.subscribe((styleSheet) => {
-            if (styleSheet.enabled) {
-                activeSheets.push(styleSheet);
-            } else {
-                const index = activeSheets.indexOf(styleSheet);
-                if (index >= 0) activeSheets.splice(index, 1);
-            } // if
-        });
-        
-        expect(activeSheets.length)
-        .toBe(0);
-        
-        sheet8.enabled = true;
-        
-        expect(activeSheets.length)
-        .toBe(0);
-        
-        sheet6.enabled = false;
-        sheet9.enabled = false;
-        
-        expect(activeSheets.length)
-        .toBe(0);
-        
-        sheet7.enabled = false;
-        
-        expect(activeSheets.length)
-        .toBe(0);
-        
-        sheet6.enabled = true;
-        
-        expect(activeSheets.length)
-        .toBe(0);
     });
 });
