0b6e7743601f2170a0d89d48f08690608d33d552
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-11 19:04:25 +0700

    updated css-types

--- packages/css-types/src/css-types.ts
+++ packages/css-types/src/css-types.ts
@@ -414,7 +414,7 @@ export type CssKeyframes               = {
 }
 export type CssKeyframesMap            = MapOf<CssKeyframes>
 export type CssKeyframesRule           = CssRule & {
-    [name: symbol] : readonly [(`@keyframes ${string}`|[`@keyframes ${string}`, undefined]), CssRuleCollection]
+    [name: symbol] : readonly [`@keyframes ${string}`, CssRuleCollection]
 }
 export type CssKeyframesRuleMap        = MapOf<CssKeyframesRule>
 
