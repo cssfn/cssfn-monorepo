a32dd0a615d077aba258840ef49d29a4a894a13b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-13 20:43:35 +0700

    updated test

--- packages/css-prop-list/__tests__/css-prop-list.test.ts
+++ packages/css-prop-list/__tests__/css-prop-list.test.ts
@@ -13,37 +13,18 @@ import {
 
 
 
-standardLonghandProps.forEach((prop) => {
-    test(`test isKnownCssProp(standardLonghandProps: ${prop})`, () => {
+standardLonghandProps.forEach((propName) => {
+    test(`test isKnownCssProp(standardLonghandProps: ${propName})`, () => {
         expect(
-            isKnownCssProp(prop)
+            isKnownCssProp(propName)
         )
         .toBe(true);
     });
 });
-standardShorthandProps.forEach((prop) => {
-    test(`test isKnownCssProp(standardShorthandProps: ${prop})`, () => {
+standardShorthandProps.forEach((propName) => {
+    test(`test isKnownCssProp(standardShorthandProps: ${propName})`, () => {
         expect(
-            isKnownCssProp(prop)
-        )
-        .toBe(true);
-    });
-});
-
-
-
-vendorLonghandProps.forEach((prop) => {
-    test(`test isKnownCssProp(vendorLonghandProps: ${prop})`, () => {
-        expect(
-            isKnownCssProp(prop)
-        )
-        .toBe(true);
-    });
-});
-vendorShorthandProps.forEach((prop) => {
-    test(`test isKnownCssProp(vendorShorthandProps: ${prop})`, () => {
-        expect(
-            isKnownCssProp(prop)
+            isKnownCssProp(propName)
         )
         .toBe(true);
     });
@@ -51,24 +32,137 @@ vendorShorthandProps.forEach((prop) => {
 
 
 
-obsoleteProps.forEach((prop) => {
-    if (prop === 'clip') return;
+vendorLonghandProps.forEach((propName) => {
+    test(`test isKnownCssProp(vendorLonghandProps: ${propName})`, () => {
+        expect(
+            isKnownCssProp(propName)
+        )
+        .toBe(true);
+    });
+});
+vendorShorthandProps.forEach((propName) => {
+    test(`test isKnownCssProp(vendorShorthandProps: ${propName})`, () => {
+        expect(
+            isKnownCssProp(propName)
+        )
+        .toBe(true);
+    });
+});
+
+
+
+const isUppercase  = (test: string) => (test >= 'A') && (test <= 'Z');
+obsoleteProps.forEach((propName) => {
+    if (propName === 'clip') return;
     
-    test(`test isKnownCssProp(obsoleteProps: ${prop})`, () => {
+    if (
+        (propName.startsWith('Moz')    && isUppercase(propName[3])) // Moz[A-Z]    => always considered valid
+        ||
+        (propName.startsWith('ms')     && isUppercase(propName[2])) // ms[A-Z]     => always considered valid
+        ||
+        (propName.startsWith('Webkit') && isUppercase(propName[6])) // Webkit[A-Z] => always considered valid
+    ) {
+        test(`test isKnownCssProp(obsoleteProps: ${propName})`, () => {
+            expect(
+                isKnownCssProp(propName)
+            )
+            .toBe(true);
+        });
+    }
+    else {
+        test(`test isKnownCssProp(obsoleteProps: ${propName})`, () => {
+            expect(
+                isKnownCssProp(propName)
+            )
+            .toBe(false);
+        });
+    } // if
+});
+
+
+
+svgProps.forEach((propName) => {
+    test(`test isKnownCssProp(svgProps: ${propName})`, () => {
         expect(
-            isKnownCssProp(prop)
+            isKnownCssProp(propName)
+        )
+        .toBe(true);
+    });
+});
+
+
+
+const shorthandProps : string[] = [
+    'foreg',
+    'backg',
+    'backgClip',
+    'anim',
+    'transf',
+    'gapX',
+    'gapY',
+    'gapInline',
+    'gapBlock',
+];
+shorthandProps.forEach((propName) => {
+    test(`test isKnownCssProp(shorthandProps: ${propName})`, () => {
+        expect(
+            isKnownCssProp(propName)
+        )
+        .toBe(true);
+    });
+});
+
+
+
+const strangeProps : string[] = [
+    'backgGrad',
+    
+    'backgThin',
+    'backgMild',
+    'backgBold',
+    'backgOutlined',
+    'backgOverwrite',
+    
+    'backgStart',
+    
+    'backgBak',
+    
+    'backgFn',
+    'backgImpt',
+    'backgCond',
+    'backgTg',
+    'backgCol',
+    
+    'backgRefs',
+    'backgDecls',
+    
+    'backg1',
+    'backg2',
+    'backg3',
+    
+    'foreground',
+    'foregrounds',
+    
+    'foregThin',
+    'foregMild',
+    'foregBold',
+    'foregOutlined',
+    'foregOverwrite',
+    
+    'foregFn',
+    'foregImpt',
+    'foregCond',
+    'foregTg',
+    'foregCol',
+    
+    'foregRefs',
+    'foregDecls',
+];
+strangeProps.forEach((propName) => {
+    test(`test isKnownCssProp(strangeProps: ${propName})`, () => {
+        expect(
+            isKnownCssProp(propName)
         )
         .toBe(false);
-    });
-});
-
-
-
-svgProps.forEach((prop) => {
-    test(`test isKnownCssProp(svgProps: ${prop})`, () => {
-        expect(
-            isKnownCssProp(prop)
-        )
-        .toBe(true);
     });
 });
