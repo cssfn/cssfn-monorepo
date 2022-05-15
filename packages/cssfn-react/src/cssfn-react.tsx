0b028216622c37b8a75cb2cfae1e4351e6cdfd2e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-15 13:16:35 +0700

    updated cssfn-react

--- packages/cssfn-react/src/cssfn-react.tsx
+++ packages/cssfn-react/src/cssfn-react.tsx
@@ -13,7 +13,6 @@ import {
     
     // hooks:
     useState,
-    useLayoutEffect,
     useEffect,
     useReducer,
     useRef,
@@ -36,22 +35,13 @@ import {
 }                           from '@cssfn/cssfn/dist/renders.js'
 
 // other libs:
-import {
-    // tests:
-    isBrowser,
-    isJsDom,
-}                           from 'is-in-browser'
-
-
-
-// utilities:
-const isClientSide : boolean = isBrowser || isJsDom;
+import type {
+    Subscription,
+}                           from 'rxjs'
 
 
 
 // hooks:
-const useIsomorphicLayoutEffect = isClientSide ? useLayoutEffect : useEffect;
-
 const triggerRenderReducer = (indices: object, newIndices: void): object => {
     return {}; // update with a new object
 };
@@ -64,27 +54,28 @@ export const useTriggerRender = () => {
 
 // react components:
 interface StyleContent {
-    rendered : string|null
+    renderedCss : string|null
 }
 interface StyleProps {
     content : StyleContent
 }
 const Style : FC<StyleProps> = memo(({ content }: StyleProps) => {
-    // because the `rendered` may a_huge_string, we need to *unreference* it:
-    const rendered = content.rendered; // copy        the props' rendered
-    content.rendered = null;           // unreference the props' rendered
+    // because the `renderedCss` may a_huge_string, we need to *unreference* it:
+    const renderedCss = content.renderedCss; // copy        the props' `renderedCss`
+    content.renderedCss = null;              // unreference the props' `renderedCss`
     
     // jsx:
+    console.log(`<Style> render!`);
     return (
         <style>
-            { rendered }
+            { renderedCss }
         </style>
     );
 });
 
 export const Styles : FC = () => {
     // states:
-    const [pendingUpdates] = useState<Set<StyleSheet>>(() => new Set());
+    const loaded           = useRef(false);
     const [styles        ] = useState<Map<StyleSheet, ReactElement<StyleProps, typeof Style>>>(() => new Map());
     const styleKeyCounter  = useRef(0);
     const triggerRender    = useTriggerRender();
@@ -92,17 +83,43 @@ export const Styles : FC = () => {
     
     
     // dom effects:
-    useIsomorphicLayoutEffect(() => {
-        // handlers:
-        const handleUpdate = (styleSheet: StyleSheet): void => {
-            pendingUpdates.add(styleSheet);
-            triggerRender();
-        };
+    const [unsubscribe   ] = useState<Subscription|undefined>(() => styleSheetRegistry.subscribe((styleSheet: StyleSheet): void => {
+        const renderedCss = (styleSheet.enabled || null) && render(styleSheet);
+        if (!renderedCss) {
+            // remove the <Style>:
+            console.log(`<Style> removed!`);
+            styles.delete(styleSheet);
+        }
+        else {
+            // add/update the <Style>:
+            console.log(`<Style> updated!`);
+            const style = styles.get(styleSheet);
+            styles.set(styleSheet,
+                /**
+                 * <Style> is a pure component and will never re-render by itself.
+                 * To update <Style>, we need to re-create <Style>.
+                 */
+                <Style
+                    content={
+                        { renderedCss } as StyleContent
+                    }
+                    
+                    key={
+                        style?.key                  // re-use the existing key
+                        ??
+                        (++styleKeyCounter.current) // generate a new key
+                    }
+                />
+            );
+        } // if
         
         
         
+        if (loaded.current) triggerRender(); // re-render the <Styles>
+    }));
+    useEffect(() => {
         // setups:
-        const unsubscribe = styleSheetRegistry.subscribe(handleUpdate);
+        loaded.current = true; // mark <Styles> component as loaded
         
         
         
@@ -112,64 +129,11 @@ export const Styles : FC = () => {
         };
     }, []); // runs once on startup
     
-    useIsomorphicLayoutEffect(() => {
-        // conditions:
-        if (!pendingUpdates.size) return;
-        
-        
-        
-        // pop the queued:
-        const styleSheets = Array.from(pendingUpdates.values());
-        pendingUpdates.clear();
-        
-        
-        
-        // render all:
-        const updates = styleSheets.map((styleSheet): readonly [StyleSheet, string|null] => [
-            styleSheet,
-            (styleSheet.enabled || null) && render(styleSheet)
-        ]);
-        
-        
-        
-        // apply all:
-        for (const [styleSheet, rendered] of updates) {
-            if (!rendered) {
-                // remove the <Style>:
-                styles.delete(styleSheet);
-            }
-            else {
-                // add/update the <Style>:
-                const style = styles.get(styleSheet);
-                styles.set(styleSheet,
-                    /**
-                     * <Style> is a pure component and will never re-render by itself.
-                     * To update <Style>, we need to re-create <Style>.
-                     */
-                    <Style
-                        content={
-                            { rendered } as StyleContent
-                        }
-                        
-                        key={
-                            style?.key                  // re-use the existing key
-                            ??
-                            (++styleKeyCounter.current) // generate a new key
-                        }
-                    />
-                );
-            } // if
-        } // for
-        
-        
-        
-        // trigger for re-render:
-        triggerRender();
-    }); // runs every render
-    
     
     
     // jsx:
+    console.log('');
+    console.log(`<Styles> render!`);
     return (
         <>
             { Array.from(styles.values()) }
