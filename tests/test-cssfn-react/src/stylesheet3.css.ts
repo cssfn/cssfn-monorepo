fef3c9ec51bc1e93d91ba53388c0ac14c77f132e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-15 11:52:29 +0700

    updated test-cssfn-react

--- tests/test-cssfn-react/src/stylesheet3.css.ts
+++ tests/test-cssfn-react/src/stylesheet3.css.ts
@@ -1,10 +1,22 @@
 import {
     styleSheet,
 } from '@cssfn/cssfn'
+import {
+    Observable,
+}                           from 'rxjs'
 
 
 
-styleSheet(() => ({
-    opacity    : 0.5,
-    visibility : 'visible',
+styleSheet(new Observable((observer) => {
+    const colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'purple'];
+    let colorIndex = 0;
+    setInterval(() => {
+        colorIndex = (colorIndex + 1) % colorList.length;
+        
+        observer.next({
+            opacity    : 0.5,
+            visibility : 'visible',
+            color      : colorList[colorIndex],
+        });
+    }, 5000);
 }), { id: 'styleSheet-3'});
