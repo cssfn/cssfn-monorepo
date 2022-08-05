cbc25201001da9d185dc83baf121eb807fc9b48b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-05 16:46:53 +0700

    updated css-types

--- packages/css-types/src/css-types.ts
+++ packages/css-types/src/css-types.ts
@@ -97,6 +97,21 @@ type ShorthandProperties = PartialNullish<{
      */
     foreg       : CssKnownBaseProps['color']
     /**
+     * Alias of **`color`**.  
+     * The **`color`** CSS property sets the foreground color value of an element's text and text decorations, and sets the `<currentcolor>` value. `currentcolor` may be used as an indirect value on _other_ properties and is the default for other color properties, such as `border-color`.
+     *
+     * **Syntax**: `<color>`
+     *
+     * **Initial value**: `canvastext`
+     *
+     * | Chrome | Firefox | Safari |  Edge  |  IE   |
+     * | :----: | :-----: | :----: | :----: | :---: |
+     * | **1**  |  **1**  | **1**  | **12** | **3** |
+     *
+     * @see https://developer.mozilla.org/docs/Web/CSS/color
+     */
+    foreground  : CssKnownBaseProps['color']
+    /**
      * Alias of **`background`**.  
      * The **`background`** shorthand CSS property sets all background style properties at once, such as color, image, origin and size, or repeat method.
      *
