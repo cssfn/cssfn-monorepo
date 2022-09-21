16e7063a60fdd7b553ab40806d04be26cfed8fe6
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-21 11:28:32 +0700

    added core

---
+++ barrels/core/__tests__/core.test.js
@@ -0,0 +1,3 @@
+test('nothing to test', () => {
+    expect(true).toBe(true);
+})
---
+++ barrels/core/src/core.ts
@@ -0,0 +1,13 @@
+// cssfn types:
+export * from '@cssfn/types'            // common typescript definitions
+export * from '@cssfn/css-types'        // CSS typescript definitions
+
+// cssfn mains:
+// `@cssfn/cssfn/src/styleSheets` => contains `styleSheetRegistry` => needs to be declared as peer dependency:
+export * from '@cssfn/cssfn'            // writes, imports, and exports css stylesheets as javascript modules
+
+// cssfn utilities:
+// `@cssfn/css-vars` => contains `globalIdCounter` => needs to be declared as peer dependency:
+export * from '@cssfn/css-vars'         // reads/writes CSS variables (CSS custom properties) in javascript property
+export * from '@cssfn/css-config'       // reads/writes CSS configuration of HTML components using centralized CSS variables
+export * from '@cssfn/css-selectors'    // manipulates css selector - parse, transform, calculate specificity, and more
