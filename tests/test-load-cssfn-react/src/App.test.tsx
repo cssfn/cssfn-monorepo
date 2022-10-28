8ab02fadd1d101df4b96751034a8b7319113ca40
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-28 21:06:14 +0700

    updated test-load-cssfn-react

--- tests/test-load-cssfn-react/src/App.test.tsx
+++ tests/test-load-cssfn-react/src/App.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('nothing to test', () => {
-  expect(true).toBe(true);
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
