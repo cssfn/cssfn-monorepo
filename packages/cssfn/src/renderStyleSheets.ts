b1f620952afa037dab9ef836c6c3c228503f8188
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-30 12:59:54 +0700

    updated renderStyleSheets

--- packages/cssfn/src/renderStyleSheets.ts
+++ packages/cssfn/src/renderStyleSheets.ts
@@ -8,6 +8,8 @@ import type {
     CssClassName,
     
     CssScopeName,
+    CssScopeEntry,
+    CssScopeMap,
 }                           from '@cssfn/css-types'
 
 // internals:
@@ -30,39 +32,41 @@ import {
 
 
 
+function convertScopeEntryToCssRule<TCssScopeName extends CssScopeName = CssScopeName>(this: CssScopeMap<TCssScopeName>, [scopeName, styles, options]: CssScopeEntry<TCssScopeName>): CssRule|null {
+    if (scopeName === '') { // globalScope => aliased to @global rule
+        return atGlobal(
+            styles
+        );
+    } // if
+    
+    
+    
+    // calculate unique class:
+    const uniqueClass    : CssClassName     = this[scopeName];
+    const uniqueSelector : CssFinalSelector = `.${uniqueClass}`;
+    
+    
+    
+    // the top level rule (scope rule):
+    return rule(
+        uniqueSelector,
+        styles,
+        { ...options, performGrouping: false }
+    );
+}
 export const renderStyleSheet = <TCssScopeName extends CssScopeName = CssScopeName>(styleSheet: StyleSheet<TCssScopeName>): string|null => {
     if (!styleSheet.enabled) return null;
     
     
     
+    //#region generate Rule(s) from factory
     const scopesFactory = styleSheet.scopes;
     const scopeList = (typeof(scopesFactory) === 'function') ? scopesFactory() : scopesFactory;
     if (!scopeList || !scopeList.length) return null;
     
-    const scopeMap     = styleSheet.classes;
-    
-    const scopeRules : CssRule[] = scopeList.map(([scopeName, styles, options]): CssRule|null => {
-        if (scopeName === '') { // globalScope => aliased to @global rule
-            return atGlobal(
-                styles
-            );
-        } // if
-        
-        
-        
-        // calculate unique class:
-        const uniqueClass    : CssClassName     = scopeMap[scopeName];
-        const uniqueSelector : CssFinalSelector = `.${uniqueClass}`;
-        
-        
-        
-        // the top level rule (scope rule):
-        return rule(
-            uniqueSelector,
-            styles,
-            { ...options, performGrouping: false }
-        );
-    }).filter((rule): rule is CssRule => !!rule);
+    const scopeMap   = styleSheet.classes;
+    const scopeRules = scopeList.map(convertScopeEntryToCssRule.bind(scopeMap));
+    //#endregion generate Rule(s) from factory
     
     
     
