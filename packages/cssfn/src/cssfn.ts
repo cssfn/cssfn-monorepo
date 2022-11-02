c045e704f612ccad0bed2daf4d757c2c4e6b8462
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-02 19:16:23 +0700

    optimize cssfn

--- packages/cssfn/src/cssfn.ts
+++ packages/cssfn/src/cssfn.ts
@@ -272,7 +272,8 @@ const createKeyframesRules = (items: CssKeyframes): CssRuleCollection => Object.
 
 
 // rule shortcuts:
-export const alwaysRule        = (styles:         CssStyleCollection                              ) => rule('&'                   , styles         );
+// export const alwaysRule       = (styles:         CssStyleCollection                              ) => rule('&'                   , styles         );
+export const alwaysRule        = (styles:         CssStyleCollection                              ) => ({ [Symbol()] : [['&', undefined], styles] }) as CssRule; // a bit faster
 export const neverRule         = (                                                                ) => rule(null                  , null           );
 export const fallbacks         = (styles:         CssStyleCollection                              ) => atRule('@fallbacks'        , styles         );
 export const fontFace          = (styles: CssFontFaceStyleCollection                              ) => atRule('@font-face'        , styles         );
