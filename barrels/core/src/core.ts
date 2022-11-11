fbdd93a485c308f236501164f8a03c9493cb424f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-11 19:58:27 +0700

    updated core

--- barrels/core/src/core.ts
+++ barrels/core/src/core.ts
@@ -11,3 +11,4 @@ export * from '@cssfn/cssfn'            // writes, imports, and exports css styl
 export * from '@cssfn/css-vars'         // reads/writes CSS variables (CSS custom properties) in javascript property
 export * from '@cssfn/css-config'       // reads/writes CSS configuration of HTML components using centralized CSS variables
 export * from '@cssfn/css-selectors'    // manipulates css selector - parse, transform, calculate specificity, and more
+export * from '@cssfn/css-supports'     // checks if a certain css feature is supported by the running browser
