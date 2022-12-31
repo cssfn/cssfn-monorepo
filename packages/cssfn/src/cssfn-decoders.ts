5a8206b4e14248ec2c9a7570dc8d4346eeb83550
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-12-31 19:08:42 +0700

    optimized cssfn-decoders

--- packages/cssfn/src/cssfn-decoders.ts
+++ packages/cssfn/src/cssfn-decoders.ts
@@ -20,79 +20,105 @@ import type {
 
 
 
-const decodeRuleData = (ruleData: EncodedCssRuleData): CssRuleData => {
-    const [selector, styles] = ruleData;
-    return [
-        selector,
-        decodeStyles(styles)
-    ];
-}
-const decodeNestedRule = (ruleData: EncodedCssRuleData): readonly [symbol, CssRuleData] => {
-    return [
-        Symbol(),
-        decodeRuleData(ruleData)
-    ];
-}
 export const decodeStyle = (style: OptionalOrBoolean<EncodedCssStyle>): OptionalOrBoolean<CssStyle> => {
     if (!style || (style === true)) return undefined; // falsy style => ignore
     
     
     
-    const ruleDatas = style['']; // an empty string key is a special property for storing (nested) rules
-    const decodedStyle = style as CssStyle; // no need to clone to improve performance
-    
-    
-    
-    if (ruleDatas && ruleDatas.length) {
+    type CssRuleEntry = readonly [symbol, CssRuleData];
+    const nestedRules : (EncodedCssRuleData|CssRuleEntry|undefined)[]|null|undefined = style['']; // an empty string key is a special property for storing (nested) rules
+    if (nestedRules && nestedRules.length) {
         // delete style[''];   // expensive op! causing chrome's to re-create hidden class
         style[''] = undefined; // assigning to undefined instead of deleting, to improve performance
         
         
         
-        const nestedRules = (
-            ruleDatas
-            .map(decodeNestedRule)
-        );
+        type MutableEncodedCssRuleData = [...EncodedCssRuleData]|[...CssRuleEntry];
+        for (let index = 0, max = nestedRules.length, encodedCssRuleData: MutableEncodedCssRuleData; index < max; index++) {
+            encodedCssRuleData = nestedRules[index] as MutableEncodedCssRuleData;
+            
+            
+            
+            const decodedStyles = decodeStyles((encodedCssRuleData as EncodedCssRuleData)[1]) // mutate EncodedCssStyleCollection with CssStyleCollection
+            if (!decodedStyles || (decodedStyles === true)) {
+                nestedRules[index] = undefined;
+            }
+            else {
+                encodedCssRuleData[1] = [         // mutate EncodedCssStyleCollection with CssRuleData
+                    (encodedCssRuleData as EncodedCssRuleData)[0], // undefined|CssRawSelector|CssFinalSelector
+                    decodedStyles                                  // CssStyleCollection
+                ] as CssRuleData;
+                encodedCssRuleData[0] = Symbol(); // mutate undefined|CssRawSelector|CssFinalSelector with symbol
+            } // if
+        } // for
+        
+        
+        
+        // cleanup blank entries:
+        for (let index = nestedRules.length - 1; index >= 0; index--) {
+            if (!nestedRules[index]) nestedRules.splice(index, 1);
+        } // for
+        
+        
+        
+        // expensive op! causing chrome's to re-create hidden class:
         Object.assign(
-            decodedStyle,
-            Object.fromEntries(nestedRules)
+            style,
+            Object.fromEntries(
+                nestedRules as CssRuleEntry[]
+            )
         );
     } // if
     
     
     
-    return decodedStyle;
+    return style as CssStyle;
 }
-function unwrapStyles(styles: Extract<EncodedCssStyleCollection, any[]>, result: CssStyle[]): void {
-    for (const style of styles) {
-        if (!style || (style === true)) continue; // falsy style(s) => ignore
+const unwrapStyles = (styles: Extract<EncodedCssStyleCollection, any[]>): void => {
+    for (let index = 0, max = styles.length, style : typeof styles[number]; index < max; index++) {
+        style = styles[index];
+        
+        
+        
+        if (!style || (style === true)) {
+            styles[index] = undefined; // mutate : falsy style => undefined (delete)
+            continue;
+        } // if
         
         
         
         // handle single item:
         if (!Array.isArray(style)) {
-            const decodedStyle = decodeStyle(style);
-            if (!decodedStyle || (decodedStyle === true)) continue; // falsy style(s) => ignore
-            result.push(decodedStyle);
+            const decodedStyle = decodeStyle(style); // mutate EncodedCssStyle with CssStyle
+            
+            
+            
+            if (!decodedStyle || (decodedStyle === true)) {
+                styles[index] = undefined; // mutate : falsy style => undefined (delete)
+                continue;
+            } // if
+            
+            
+            
+            styles[index] = decodedStyle as CssStyle as any; // mutate EncodedCssStyle with CssStyle
             continue;
         } // if
         
         
         
         // handle multi item(s):
-        unwrapStyles(style, result);
+        unwrapStyles(style); // mutate each EncodedCssStyle with CssStyle
     } // for
 }
 export const decodeStyles = (styles: EncodedCssStyleCollection): CssStyleCollection => {
     // statically handle single item:
     if (!Array.isArray(styles)) {
-        return decodeStyle(styles);
+        return decodeStyle(styles); // mutate EncodedCssStyle with CssStyle
     } // if
     
     
     
     // dynamically handle multi item(s):
-    const result: CssStyle[] = [];
-    unwrapStyles(styles, result);
-    return result;
+    unwrapStyles(styles); // mutate each EncodedCssStyle with CssStyle
+    return styles as CssStyle[];
 }
