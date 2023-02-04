8a4088df6366d2dd72bf035ead143cdf0aa31b3a
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-04 16:53:50 +0700

    updated implementation of lazyCsr

--- packages/cssfn-dom/src/cssfn-dom.ts
+++ packages/cssfn-dom/src/cssfn-dom.ts
@@ -59,8 +59,8 @@ export const config = { asyncRender: true };
 
 
 // dom:
-const headElement = isClientSide ? document.head : undefined;
-const styleElms = new Map<StyleSheet, HTMLStyleElement|null>(); // map a relationship between StyleSheet_object => (SSR|CSR) HTMLStyleElement
+const headElement  = isClientSide ? document.head : undefined;
+const csrStyleElms = new Map<StyleSheet, HTMLStyleElement|null>(); // map a relationship between StyleSheet_object => (SSR|CSR) HTMLStyleElement
 
 const findCssfnStyleElmById = (cssfnId: string): HTMLStyleElement|null => {
     if (!cssfnId) return null; // empty string => return null
@@ -84,70 +84,105 @@ const batchCommit = () => {
     
     
     // apply the changes:
-    const batchAppendChildren : HTMLStyleElement[] = [];
-    const batchDeleteChildren : HTMLStyleElement[] = [];
+    const batchAppendChildren : HTMLStyleElement[]                      = [];
+    const batchDeleteChildren : HTMLStyleElement[]                      = [];
+    const batchMutateChildren : (readonly [HTMLStyleElement, string])[] = [];
     for (const [styleSheet, rendered] of changes) {
         if (!rendered) {
             // remove the styleSheet:
             
+            
+            
+            // find the SSR/CSR generated <style> element (if any):
             const styleElm = (
-                styleElms.get(styleSheet)
+                // find the CSR generated <style> element (if any):
+                csrStyleElms.get(styleSheet)
                 ??
-                // find the SSR generated element (if any):
+                // find the SSR generated <style> element (if any):
                 findCssfnStyleElmById(styleSheet.id)
             );
-            if (styleElm) {
-                styleElms.set(styleSheet, null); // MARK the related HTMLStyleElement as DELETED, so we still have information that the styleSheet EVER CSR generated
+            if (styleElm) { // if found => delete it
+                // delete existing relationship between StyleSheet_object => SSR/CSR HTMLStyleElement:
+                csrStyleElms.set(styleSheet, null /* = no corresponding HTMLStyleElement */); // MARK the related HTMLStyleElement as DELETED, so we still have information that the styleSheet EVER CSR generated
                 
+                // schedule to delete existing <style> element:
                 batchDeleteChildren.push(styleElm);
             } // if
         }
         else {
             // add/update the styleSheet:
             
-            let styleElm = styleElms.get(styleSheet);
-            if (!styleElm) {
+            
+            
+            // find the CSR generated <style> element (if any):
+            let styleElm = csrStyleElms.get(styleSheet);
+            if (!styleElm) { // if not found => (1st priority) re-use SSR generated <style> element -or- (2nd priority) generate a new one
                 // add the styleSheet:
                 
-                // find the SSR generated element (if any):
-                const existingStyleElm = findCssfnStyleElmById(styleSheet.id);
                 
-                styleElm = (
-                    // re-use the existing <style> element (if any):
-                    existingStyleElm
-                    ??
-                    // create a new <style> element:
-                    document.createElement('style')
-                );
-                styleElms.set(styleSheet, styleElm); // make a relationship between StyleSheet_object => CSR HTMLStyleElement
                 
-                if (!existingStyleElm) {
+                // find the SSR generated <style> element (if any):
+                const ssrStyleElm = findCssfnStyleElmById(styleSheet.id);
+                if (ssrStyleElm) { // found SSR generated <style> element => re-use it
+                    /* ***** [greedyCsr]: always replace the content of SSR generated <style> element ***** */
+                    
+                    
+                    
+                    // make a relationship between StyleSheet_object => SSR HTMLStyleElement:
+                    csrStyleElms.set(styleSheet, ssrStyleElm);
+                    
+                    // schedule to re-use (update) SSR generated <style> element:
+                    batchMutateChildren.push([ssrStyleElm, rendered]); // replace: SSR generated => CSR generated
+                }
+                else {
+                    /* ***** create a new CSR generated <style> element ***** */
+                    
+                    
+                    
+                    styleElm = document.createElement('style'); // create a new <style> element
+                    styleElm.textContent = rendered;
                     styleElm.dataset.cssfnId = styleSheet.id || '';
+                    
+                    // make a relationship between StyleSheet_object => CSR HTMLStyleElement:
+                    csrStyleElms.set(styleSheet, styleElm);
+                    
+                    // schedule to append a new CSR generated <style> element:
                     batchAppendChildren.push(styleElm);
-                };
-                
-                
-                
-                // update the styleSheet:
-                styleElm.textContent = rendered; // CSR generated
+                } // if
             }
             else {
                 // update the styleSheet:
                 
-                styleElm.textContent = rendered; // CSR generated
+                
+                
+                // schedule to re-use (update) CSR generated <style> element:
+                batchMutateChildren.push([styleElm, rendered]); // CSR generated
             } // if
         } // if
     } // for
     
     
     
-    //#region efficiently mutate(s) the head
-    // add children:
+    //#region efficiently mutate(s) the <head> element
+    // mutate children:
+    // the first operation, the <style> element(s) was already there, so the operation is fast & instantly applied
+    for (const [styleElm, rendered] of batchMutateChildren) {
+        styleElm.textContent = rendered;
+    } // for
+    
+    
+    
+    // append children:
+    // the second operation, because it's quite heavy task to append a new <style> element(s)
     if (batchAppendChildren.length) {
         if (batchAppendChildren.length === 1) {
+            // singular append:
+            
             headElement?.appendChild(batchAppendChildren[0]);
         }
         else {
+            // plural append:
+            
             const childrenGroup = document.createDocumentFragment();
             for (const styleElm of batchAppendChildren) {
                 childrenGroup.appendChild(styleElm);
@@ -159,10 +194,11 @@ const batchCommit = () => {
     
     
     // delete children:
+    // the last operation, because the excess <style> element(s) have no significant visual impact
     for (const styleElm of batchDeleteChildren) {
         styleElm.parentElement?.removeChild(styleElm);
     } // for
-    //#endregion efficiently mutate(s) the head
+    //#endregion efficiently mutate(s) the <head> element
 }
 
 
@@ -192,12 +228,17 @@ const scheduleBatchCommit = () => {
 
 // handlers:
 const handleUpdate = async (styleSheet: StyleSheet): Promise<void> => {
-    if (!styleSheet.greedyCsr) {          // if possible, skip the first_render when the SSR <style> is available
-        if (!styleElms.has(styleSheet)) { // if the styleSheet NEVER CSR generated => find the SSR generated (if any)
-            // find the SSR generated element (if any):
-            const existingStyleElm = findCssfnStyleElmById(styleSheet.id);
-            if (existingStyleElm) {
-                styleElms.set(styleSheet, existingStyleElm); // make a relationship between StyleSheet_object => SSR HTMLStyleElement
+    if (styleSheet.lazyCsr) {
+        /* ***** [lazyCsr]: if possible, skip the first_render by re-use SSR generated <style> element ***** */
+        
+        
+        
+        if (!csrStyleElms.has(styleSheet)) { // if the styleSheet is NEVER CSR generated => find the SSR generated (if any)
+            // find the SSR generated <style> element (if any):
+            const ssrStyleElm = findCssfnStyleElmById(styleSheet.id);
+            if (ssrStyleElm) { // found SSR generated <style> element => re-use it
+                // make a relationship between StyleSheet_object => SSR HTMLStyleElement:
+                csrStyleElms.set(styleSheet, ssrStyleElm);
                 
                 
                 
@@ -244,11 +285,11 @@ if (headElement) { // === if (isClientSide)
         // register a callback on the next macro_task (just AFTER the first_paint occured):
         const messageChannel = new MessageChannel();
         messageChannel.port1.onmessage = () => {
-            // wait for 10 seconds to remove all <style>(s) having [data-cssfn-id] attr in which not listed in `styleElms`:
+            // wait for 10 seconds to remove all <style>(s) having [data-cssfn-id] attr in which not listed in `csrStyleElms`:
             setTimeout(() => {
-                // remove all <style>(s) having [data-cssfn-id] attr in which not listed in `styleElms`:
+                // remove all <style>(s) having [data-cssfn-id] attr in which not listed in `csrStyleElms`:
                 const cssfnStyles           = Array.from(headElement.querySelectorAll('style[data-cssfn-id]')) as HTMLStyleElement[];
-                const registeredCssfnStyles = new Set<HTMLStyleElement|null>(styleElms.values());
+                const registeredCssfnStyles = new Set<HTMLStyleElement|null>(csrStyleElms.values());
                 const unusedCssfnStyles     = cssfnStyles.filter((cssfnStyle) => !registeredCssfnStyles.has(cssfnStyle));
                 for (const unusedCssfnStyle of unusedCssfnStyles) {
                     unusedCssfnStyle.parentElement?.removeChild(unusedCssfnStyle);
