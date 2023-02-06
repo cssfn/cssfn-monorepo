133f2d7d5665994c38b10db716eeec2e7263c3e4
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-06 17:46:27 +0700

    updated test

--- tests/test-cssfn-react/src/stylesheet8_css.ts
+++ tests/test-cssfn-react/src/stylesheet8_css.ts
@@ -5,5 +5,5 @@ import {
 
 
 export const useStyleSheet8 = dynamicStyleSheet(
-    import(/* webpackChunkName: "stylesheet8_css_dyn" */ './stylesheet8_css_dyn')
+    () => import(/* webpackChunkName: "stylesheet8_css_dyn" */ './stylesheet8_css_dyn')
 , { id: 'styleSheet-8'});
