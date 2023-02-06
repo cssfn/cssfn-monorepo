af34c708459fcc41b3a128d7d898e1a7080de3a5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-06 13:43:04 +0700

    updated types

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -1,18 +1,59 @@
+// optionals:
 export type Optional<T>                    = T|null|undefined
 export type OptionalOrBoolean<T>           = Optional<T|boolean>
-export type SingleOrArray<T>               = T|T[]
 
-export type DeepArray<T>                   = (T|DeepArray<T>)[] // ===       T[]  |  T[][]  |  T[][][]  |  ...
-export type SingleOrDeepArray<T>           = T|DeepArray<T>     // === T  |  T[]  |  T[][]  |  T[][][]  |  ...
 
+
+// arrays:
+export type MaybeArray<T>                  = T|Array<T>
+/**
+ * @deprecated renamed to `MaybeArray<T>`
+ */
+export type SingleOrArray<T>               = MaybeArray<T>
+
+export type DeepArray<T>                   = Array<T|DeepArray<T>> // recursive
+export type MaybeDeepArray<T>              = T|DeepArray<T>
+/**
+ * @deprecated renamed to `MaybeDeepArray<T>`
+ */
+export type SingleOrDeepArray<T>           = MaybeDeepArray<T>
+
+
+
+// promises:
 export type MaybePromise<T>                = T|Promise<T>
+
+
+
+// modules:
 export type ModuleDefault<T>               = { default: T } // only accept the default export, ignores named exports
 export type MaybeModuleDefault<T>          = T|ModuleDefault<T>
-export type Factory<T>                     = () => T
-export type ProductOrFactory<T>            = T|Factory<T>
-export type ProductOrFactoryDeepArray<T>   = (ProductOrFactory<T> | ProductOrFactoryDeepArray<T>)[] // ===         T[]|F[]  |  T[][]|F[][]  |  T[][][]|F[][][]  |  ...
-export type ProductOrFactoryOrDeepArray<T> =  ProductOrFactory<T> | ProductOrFactoryDeepArray<T>    // === T|F  |  T[]|F[]  |  T[][]|F[][]  |  T[][][]|F[][][]  |  ...
 
+
+
+// factories:
+export type Factory<T>                     = () => T
+export type MaybeFactory<T>                = T|Factory<T>
+/**
+ * @deprecated renamed to `MaybeFactory<T>`
+ */
+export type ProductOrFactory<T>            = MaybeFactory<T>
+
+export type MaybeFactoryDeepArray<T>       = Array<MaybeFactory<T> | MaybeFactoryDeepArray<T>> // recursive
+/**
+ * @deprecated renamed to `MaybeFactoryDeepArray<T>`
+ */
+export type ProductOrFactoryDeepArray<T>   = MaybeFactoryDeepArray<T>
+
+export type MaybeFactoryMaybeDeepArray<T>  = MaybeFactory<T> | MaybeFactoryDeepArray<T>
+/**
+ * @deprecated renamed to `MaybeFactoryMaybeDeepArray<T>`
+ */
+export type ProductOrFactoryOrDeepArray<T> = MaybeFactoryMaybeDeepArray<T>
+
+
+
+// dictionaries/maps:
 export type PartialNullish<T>              = { [Key in keyof T] ?: null |  T[Key]                  }
 export type RequiredNotNullish<T>          = { [Key in keyof T]  : Exclude<T[Key], undefined|null> }
 
