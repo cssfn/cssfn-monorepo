04877984f43a557732215195a05931341aa5dbaf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-06 15:09:12 +0700

    updated types

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -6,17 +6,9 @@ export type OptionalOrBoolean<T>           = Optional<T|boolean>
 
 // arrays:
 export type MaybeArray<T>                  = T|Array<T>
-/**
- * @deprecated renamed to `MaybeArray<T>`
- */
-export type SingleOrArray<T>               = MaybeArray<T>
 
 export type DeepArray<T>                   = Array<T|DeepArray<T>> // recursive
 export type MaybeDeepArray<T>              = T|DeepArray<T>
-/**
- * @deprecated renamed to `MaybeDeepArray<T>`
- */
-export type SingleOrDeepArray<T>           = MaybeDeepArray<T>
 
 
 
@@ -28,31 +20,15 @@ export type MaybePromise<T>                = T|Promise<T>
 // factories:
 export type Factory<out T>                 = () => T
 export type MaybeFactory<T>                = T|Factory<T>
-/**
- * @deprecated renamed to `MaybeFactory<T>`
- */
-export type ProductOrFactory<T>            = MaybeFactory<T>
 
 export type MaybeFactoryDeepArray<T>       = Array<MaybeFactory<T> | MaybeFactoryDeepArray<T>> // recursive
-/**
- * @deprecated renamed to `MaybeFactoryDeepArray<T>`
- */
-export type ProductOrFactoryDeepArray<T>   = MaybeFactoryDeepArray<T>
 
 export type MaybeFactoryMaybeDeepArray<T>  = MaybeFactory<T> | MaybeFactoryDeepArray<T>
-/**
- * @deprecated renamed to `MaybeFactoryMaybeDeepArray<T>`
- */
-export type ProductOrFactoryOrDeepArray<T> = MaybeFactoryMaybeDeepArray<T>
 
 
 
 // modules:
 export type ModuleDefault<out T>           = { default: T } // only accept the default export, ignores named exports
-/**
- * @deprecated please upgrade to `MaybeLazyModuleDefault<T>`, because non_lazy of dynamic_module doesn't make sense
- */
-export type MaybeModuleDefault<T>          = T|ModuleDefault<T>
 export type LazyModuleDefault<out T>       = Factory<Promise<ModuleDefault<T>>>
 export type MaybeLazyModuleDefault<T>      = T|LazyModuleDefault<T>
 
