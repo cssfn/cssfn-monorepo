5812f58c8adfd48d0033f39f1b3e13a999a275ea
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-06 20:08:56 +0700

    updated css-types

--- packages/css-types/src/css-types.ts
+++ packages/css-types/src/css-types.ts
@@ -384,31 +384,28 @@ export type CssFontFaceProps =
     & CssLonghandFontFaceProps
     & CssShorthandFontFaceProps
 
-export type CssPropertyProps =
-    & {
-        syntax       :
-            |'<length>'
-            |'<number>'
-            |'<percentage>'
-            |'<length-percentage>'
-            |'<color>'
-            |'<image>'
-            |'<url>'
-            |'<integer>'
-            |'<angle>'
-            |'<time>'
-            |'<resolution>'
-            |'<transform-function>'
-            |'<custom-ident>'
-            |'<transform-list>'
-            |'*'
-            |(string & {})
-        
-        inherits     : boolean
-    }
-    & PartialNullish<{
-        initialValue : string
-    }>
+export type CssPropertyProps = PartialNullish<{
+    syntax       :
+        |'<length>'
+        |'<number>'
+        |'<percentage>'
+        |'<length-percentage>'
+        |'<color>'
+        |'<image>'
+        |'<url>'
+        |'<integer>'
+        |'<angle>'
+        |'<time>'
+        |'<resolution>'
+        |'<transform-function>'
+        |'<custom-ident>'
+        |'<transform-list>'
+        |'*'
+        |(string & {})
+    
+    inherits     : boolean
+    initialValue : string
+}>
 //#endregion css special properties
 
 export type CssKnownStandardLonghandProps  =
