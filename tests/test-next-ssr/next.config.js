8f48d2230357a2cb4bc350b42804ce2eb3ad3357
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-06 23:28:34 +0700

    updated tests

--- tests/test-next-ssr/next.config.js
+++ tests/test-next-ssr/next.config.js
@@ -17,4 +17,9 @@ const nextConfig = {
     },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true,
+});
+module.exports = withBundleAnalyzer(nextConfig);
+
+// module.exports = nextConfig
